3743b8e31ecdc17a06301cd40b9bff86ddd326f5
Falin4789 <koo7264@gmail.com>
2024-07-07 17:42:27 +0900

    Add files via upload

---
+++ library/src/main/java/com/example/library/Config/WebConfig.java
@@ -0,0 +1,9 @@
+package com.example.library.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    // 인터셉터 설정을 제거합니다.
+}
