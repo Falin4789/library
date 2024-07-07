f8ebb1ca7067745ee814e24d6b1cee7cc6518821
Falin4789 <koo7264@gmail.com>
2024-07-08 08:39:46 +0900

    Delete library directory

--- library/src/main/java/com/example/library/exception/PostNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.example.library.exception;
-
-public class PostNotFound extends RuntimeException {
-    public PostNotFound(Long id) {
-        super("Post ID not found: " + id);
-    }
-}
